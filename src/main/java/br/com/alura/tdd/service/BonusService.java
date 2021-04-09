a90f680d9701a87c0305e53da1f47e6401123f0f
Rinaldo Medeiros <rinaldo.rmedeiros@gmail.com>
2021-04-09 17:34:55 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -0,0 +1,19 @@
+package br.com.alura.tdd.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+public class BonusService {
+
+	public BigDecimal calcularBonus(Funcionario funcionario) {
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		if (valor.compareTo(new BigDecimal("1000")) > 0) {
+//			valor = BigDecimal.ZERO;
+			throw new IllegalArgumentException("Funcionário com salário mair que R$ 10.000,00 não pode receber bônus");
+		}
+		return valor.setScale(2,RoundingMode.HALF_UP );
+	}
+
+}
